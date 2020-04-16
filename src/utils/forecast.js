dc22d43f313439995e46a2d7bb79d0446a13229c
Vinayak Pillai <vinayakpillai90@gmail.com>
2020-04-16 12:52:15 +0530

    Forecast changes done

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find the area', undefined)
 
         }else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability +   ' % chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability +   ' % chance of rain. The wind speed is ' + body.daily.data[0].windSpeed)
         }
      
 
